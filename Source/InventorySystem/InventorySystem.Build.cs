806ecfc2a2e61534fedecd7c10c7eeeda68b42f6
robkohout <robkohout@hotmail.com>
2025-06-07 18:15:34 -0700

    Project Creation and Assets

---
+++ Source/InventorySystem/InventorySystem.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class InventorySystem : ModuleRules
+{
+	public InventorySystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"InventorySystem",
+			"InventorySystem/Variant_Platforming",
+			"InventorySystem/Variant_Combat",
+			"InventorySystem/Variant_Combat/AI",
+			"InventorySystem/Variant_SideScrolling",
+			"InventorySystem/Variant_SideScrolling/Gameplay",
+			"InventorySystem/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/InventorySystem/InventorySystemGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InventorySystemGameMode.h"
+
+AInventorySystemGameMode::AInventorySystemGameMode()
+{
+	// stub
+}
