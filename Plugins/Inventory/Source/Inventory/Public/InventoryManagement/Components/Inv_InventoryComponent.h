52b22bc338dc6d4df2f5fef895c138be4197294f
robkohout <robkohout@hotmail.com>
2025-06-08 16:29:05 -0700

    Fast Array Functions

--- Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -7,7 +7,10 @@
 #include "Inv_InventoryComponent.generated.h"
 
 
+class UInv_InventoryItem;
 class UInv_InventoryBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInventoryItemChange, UInv_InventoryItem*, Item);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
 class INVENTORY_API UInv_InventoryComponent : public UActorComponent
@@ -18,6 +21,9 @@ public:
 	UInv_InventoryComponent();
 
 	void ToggleInventoryMenu();
+
+	FInventoryItemChange OnItemAdded;
+	FInventoryItemChange OnItemRemoved;
 	
 protected:
 	virtual void BeginPlay() override;
