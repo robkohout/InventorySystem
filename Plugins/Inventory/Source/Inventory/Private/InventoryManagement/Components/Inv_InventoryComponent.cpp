3288e7313dfda576d2b24e06a88b334514062425
robkohout <robkohout@hotmail.com>
2025-06-09 20:06:13 -0700

    Has Room for Item

--- Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
@@ -14,7 +14,15 @@ UInv_InventoryComponent::UInv_InventoryComponent()
 
 void UInv_InventoryComponent::TryAddItem(UInv_ItemComponent* ItemComponent)
 {
-	NoRoomInInventory.Broadcast();
+	FInv_SlotAvailabilityResult Result = InventoryMenu->HasRoomForItem(ItemComponent);
+
+	if (Result.TotalRoomToFill == 0)
+	{
+		NoRoomInInventory.Broadcast();
+		return;
+	}
+
+	// TODO: Actually add the item to the inventory
 }
 
 void UInv_InventoryComponent::ToggleInventoryMenu()
--- Plugins/Inventory/Source/Inventory/Public/Types/Inv_GridTypes.h
+++ Plugins/Inventory/Source/Inventory/Public/Types/Inv_GridTypes.h
@@ -4,6 +4,8 @@
 
 #include "Inv_GridTypes.generated.h"
 
+class UInv_InventoryItem;
+
 UENUM(BlueprintType)
 enum class EInv_ItemCategory : uint8
 {
@@ -11,4 +13,31 @@ enum class EInv_ItemCategory : uint8
 	Consumable,
 	Craftable,
 	None
+};
+
+USTRUCT()
+struct FInv_SlotAvailability
+{
+	GENERATED_BODY()
+
+	FInv_SlotAvailability() {}
+	FInv_SlotAvailability(int32 ItemIndex, int32 Room, bool bHasItem) : Index(ItemIndex), AmountToFill(Room), bItemAtIndex(bHasItem) {}
+	
+	int32 Index{INDEX_NONE};
+	int32 AmountToFill{0};
+	bool bItemAtIndex{false};
+};
+
+USTRUCT()
+struct FInv_SlotAvailabilityResult
+{
+	GENERATED_BODY()
+
+	FInv_SlotAvailabilityResult() {}
+
+	TWeakPtr<UInv_InventoryItem> Item;
+	int32 TotalRoomToFill{0};
+	int32 Remainder{0};
+	bool bStackable{false};
+	TArray<FInv_SlotAvailability> SlotAvailabilities;
 };
