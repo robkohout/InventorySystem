6642e7b0c23d82e72647cfda1852dd01e215125a
robkohout <robkohout@hotmail.com>
2025-06-11 19:22:06 -0700

    Item Type Tags

---
+++ Plugins/Inventory/Source/Inventory/Private/Items/Inv_ItemTags.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright Rob Kohout
+
+
+#include "Items/Inv_ItemTags.h"
+
+namespace GameItems
+{
+	namespace Equipment
+	{
+		namespace Weapons
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Axe, "GameItems.Equipment.Weapons.Axe", "Axe")
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Sword, "GameItems.Equipment.Weapons.Sword", "Sword")
+		}
+
+		namespace Cloaks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(RedCloak, "GameItems.Equipment.Cloaks.RedCloak", "RedCloak")
+		}
+
+		namespace Masks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(SteelMask, "GameItems.Equipment.Masks.SteelMask", "SteelMask")
+		}
+	}
+
+	namespace Consumables
+	{
+		namespace Potions
+		{
+			namespace Red
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Consumables.Potions.Red.Small", "Small")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Consumables.Potions.Red.Large", "Large")
+			}
+
+			namespace Blue
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Consumables.Potions.Blue.Small", "Small")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Consumables.Potions.Blue.Large", "Large")
+			}
+		}
+	}
+
+	namespace Craftables
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FireFernFruit, "GameItems.Craftables.FireFernFruit", "FireFernFruit")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(LuminDaisy, "GameItems.Craftables.LuminDaisy", "LuminDaisy")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ScorchPetalBlossom, "GameItems.Craftables.ScorchPetalBlossom", "ScorchPetalBlossom")
+	}
+}
--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Types/Inv_GridTypes.h"
 #include "StructUtils/InstancedStruct.h"
+#include "GameplayTagContainer.h"
 #include "Inv_ItemManifest.generated.h"
 
 /*
@@ -21,9 +22,13 @@ struct INVENTORY_API FInv_ItemManifest
 
 public:
 	UInv_InventoryItem* Manifest(UObject* NewOuter);
-	EInv_ItemCategory GetItemCategory() const { return ItemCategory; };
+	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
+	FGameplayTag GetItemType() const { return ItemType; }
 	
 private:
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	EInv_ItemCategory ItemCategory{EInv_ItemCategory::None};
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag ItemType;
 };
