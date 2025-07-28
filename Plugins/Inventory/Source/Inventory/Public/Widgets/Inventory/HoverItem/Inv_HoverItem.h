217d203db156803c8ac6f2761bb2c0e2fcff78f4
robkohout <robkohout@hotmail.com>
2025-07-27 18:43:29 -0700

    Hover Item

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/HoverItem/Inv_HoverItem.h
@@ -0,0 +1,49 @@
+﻿// Copyright Rob Kohout
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_HoverItem.generated.h"
+
+class UTextBlock;
+class UInv_InventoryItem;
+class UImage;
+/**
+ * The HoverItem is the item that will appear and follow the mouse
+ * when an inventory item on the grid has been clicked.
+ */
+UCLASS()
+class INVENTORY_API UInv_HoverItem : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetImageBrush(const FSlateBrush& Brush) const;
+	void UpdateStackCount(const int32 Count) const;
+
+	FGameplayTag GetItemType() const;
+	int32 GetStackCount() const { return StackCount; }
+	bool IsStackable() const { return bIsStackable; }
+	void SetIsStackable(bool bStacks);
+	int GetPreviousGridIndex() const { return PreviousGridIndex; }
+	void SetPreviousGridIndex(int32 Index) { PreviousGridIndex = Index; }
+	FIntPoint GetGridDimensions() const { return GridDimensions; }
+	void SetGridDimensions(const FIntPoint& Dimensions) { GridDimensions = Dimensions; }
+	UInv_InventoryItem* GetInventoryItem() const;
+	void SetInventoryItem(UInv_InventoryItem* Item);
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Icon;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_StackCount;
+		
+	int32 PreviousGridIndex;
+	FIntPoint GridDimensions;
+	TWeakObjectPtr<UInv_InventoryItem> InventoryItem;
+	bool bIsStackable{false};
+	int32 StackCount{0};
+};
