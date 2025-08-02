0831fd8f9187e4556615d476c25c05e13e51cb00
robkohout <robkohout@hotmail.com>
2025-08-01 22:31:03 -0700

    PopUp Menu Class, Widget Events

---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/Inv_ItemPopUp.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright Rob Kohout
+
+
+#include "Widgets/ItemPopUp/Inv_ItemPopUp.h"
+#include "Components/Button.h"
+#include "Components/Slider.h"
+
+void UInv_ItemPopUp::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	Button_Split->OnClicked.AddDynamic(this, &ThisClass::SplitButtonClicked);
+	Button_Drop->OnClicked.AddDynamic(this, &ThisClass::DropButtonClicked);
+	Button_Consume->OnClicked.AddDynamic(this, &ThisClass::ConsumeButtonClicked);
+	Slider_Split->OnValueChanged.AddDynamic(this, &ThisClass::SliderValueChanged);
+}
+
+void UInv_ItemPopUp::SplitButtonClicked()
+{
+	
+}
+
+void UInv_ItemPopUp::DropButtonClicked()
+{
+	
+}
+
+void UInv_ItemPopUp::ConsumeButtonClicked()
+{
+	
+}
+
+void UInv_ItemPopUp::SliderValueChanged(float Value)
+{
+	
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -0,0 +1,55 @@
+﻿// Copyright Rob Kohout
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_ItemPopUp.generated.h"
+
+class USizeBox;
+class UTextBlock;
+class USlider;
+class UButton;
+/**
+ * The item popup widget shows up when right-clicking on an item
+ * in the inventory grid
+ */
+UCLASS()
+class INVENTORY_API UInv_ItemPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Split;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Drop;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consume;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> Slider_Split;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_SplitAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+
+	UFUNCTION()
+	void SplitButtonClicked();
+
+	UFUNCTION()
+	void DropButtonClicked();
+
+	UFUNCTION()
+	void ConsumeButtonClicked();
+
+	UFUNCTION()
+	void SliderValueChanged(float Value);
+};
