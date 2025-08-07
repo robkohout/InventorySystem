1d4d134c9a928741169c230b18568624dfb9f8ba
Rob Kohout <robkohout@hotmail.com>
2025-08-07 11:22:19 -0500

    Item Description Widget

---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemDescription/Inv_ItemDescription.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Rob Kohout
+
+
+#include "Widgets/ItemDescription/Inv_ItemDescription.h"
+
+#include "Components/SizeBox.h"
+
+FVector2D UInv_ItemDescription::GetBoxSize() const
+{
+	return SizeBox->GetDesiredSize();
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemDescription/Inv_ItemDescription.h
@@ -0,0 +1,24 @@
+﻿// Copyright Rob Kohout
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_ItemDescription.generated.h"
+
+class USizeBox;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_ItemDescription : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	FVector2D GetBoxSize() const;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox;
+};
