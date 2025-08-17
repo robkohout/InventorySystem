99e5f98da1c04b84322237410fb3cd2f97cb6bc1
robkohout <robkohout@hotmail.com>
2025-08-16 18:47:26 -0700

    Character Display

--- Plugins/Inventory/Source/Inventory/Public/EquipmentManagement/ProxyMesh/Inv_ProxyMesh.h
+++ Plugins/Inventory/Source/Inventory/Public/EquipmentManagement/ProxyMesh/Inv_ProxyMesh.h
@@ -15,7 +15,8 @@ class INVENTORY_API AInv_ProxyMesh : public AActor
 
 public:
 	AInv_ProxyMesh();
-
+	USkeletalMeshComponent* GetMesh() const { return Mesh; }
+	
 protected:
 	virtual void BeginPlay() override;
 
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/CharacterDisplay/Inv_CharacterDisplay.h
@@ -0,0 +1,30 @@
+﻿// Copyright Rob Kohout
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_CharacterDisplay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_CharacterDisplay : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnInitialized() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	
+private:
+	bool bIsDragging{false};
+	TWeakObjectPtr<USkeletalMeshComponent> Mesh;
+
+	FVector2D CurrentPosition;
+	FVector2D LastPosition;
+};
